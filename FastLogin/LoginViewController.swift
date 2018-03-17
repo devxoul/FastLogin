c14bb9388d51e5958c03126e14f9cd94fd183e8c
Suyeol Jeon <devxoul@gmail.com>
2018-03-17 15:49:10 +0900

    Initial commit :)

---
+++ FastLogin/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FastLogin
+//
+//  Created by Suyeol Jeon on 17/03/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
