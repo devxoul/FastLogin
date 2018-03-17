9705b62cfee862d776138c00ab69d49d33fd7eda
Suyeol Jeon <devxoul@gmail.com>
2018-03-18 04:00:07 +0900

    AuthService 작성

---
+++ FastLogin/AuthService.swift
@@ -0,0 +1,55 @@
+//
+//  Authenticator.swift
+//  FastLogin
+//
+//  Created by Suyeol Jeon on 18/03/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import Alamofire
+import CodableAlamofire
+
+enum LoginResult {
+  case success
+  case failure(LoginError)
+}
+
+enum LoginError: Error {
+  case wrongUsername
+  case wrongPassword
+}
+
+final class AuthService {
+  static let shared = AuthService()
+
+  func login(username: String?, password: String?, completion: @escaping (LoginResult) -> Void) {
+    guard let username = username, !username.isEmpty else { return completion(.failure(.wrongUsername)) }
+    guard let password = password, !password.isEmpty else { return completion(.failure(.wrongPassword)) }
+
+    let parameters: Parameters = [
+      "username": username,
+      "password": password,
+    ]
+
+    Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters)
+      .validate()
+      .responseDecodableObject(keyPath: "form") { (response: DataResponse<LoginResponse>) in
+        let credential = response.value.map { ($0.username, $0.password) }
+        switch credential {
+        case ("fast", "campus")?:
+          completion(.success)
+
+        case ("fast", _)?:
+          completion(.failure(.wrongPassword))
+
+        default:
+          completion(.failure(.wrongUsername))
+        }
+      }
+  }
+}
+
+private struct LoginResponse: Decodable {
+  let username: String
+  let password: String
+}
