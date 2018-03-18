8b5b8619945241f5fae41860a631dcb06f4e51d1
Suyeol Jeon <devxoul@gmail.com>
2018-03-18 19:53:19 +0900

    UserService 작성

---
+++ FastLogin/UserService.swift
@@ -0,0 +1,27 @@
+//
+//  UserService.swift
+//  FastLogin
+//
+//  Created by Suyeol Jeon on 18/03/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import Alamofire
+import CodableAlamofire
+
+final class UserService {
+  static let shared = UserService()
+
+  func currentUser(completion: @escaping (Result<String>) -> Void) {
+    Alamofire.request("https://httpbin.org/get?username=fast", method: .get)
+      .validate()
+      .responseDecodableObject(keyPath: "args") { (response: DataResponse<CurrentUserResponse>) in
+        let result = response.result.map { $0.username }
+        completion(result)
+      }
+  }
+}
+
+private struct CurrentUserResponse: Decodable {
+  let username: String
+}
