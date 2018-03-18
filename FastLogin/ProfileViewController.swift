dc95d4b01fcbad99fc84bb6a80978c975dffe5e7
Suyeol Jeon <devxoul@gmail.com>
2018-03-18 19:31:34 +0900

    프로필 화면 UI 구성

---
+++ FastLogin/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  FastLogin
+//
+//  Created by Suyeol Jeon on 18/03/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+  @IBOutlet var welcomeLabel: UILabel!
+
+  @IBAction private func logout() {
+  }
+}
