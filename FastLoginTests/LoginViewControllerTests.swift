a1896724b78cfee1f0106a38d6d8cf7eb97db626
Suyeol Jeon <devxoul@gmail.com>
2018-03-24 16:09:01 +0900

    로그인 기능 테스트 작성

--- FastLoginTests/LoginViewControllerTests.swift
+++ FastLoginTests/LoginViewControllerTests.swift
@@ -21,11 +21,63 @@ class LoginViewControllerTests: XCTestCase {
   }
 
   func testLoginSuccess_changeWindowRootViewController() {
+    // given
+    _ = self.viewController.view // loadView
+    self.viewController.usernameField.text = "fast"
+    self.viewController.passwordField.text = "campus"
+
+    // when
+    self.viewController.login()
+
+    // wait
+    let expectation = XCTestExpectation()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: expectation.fulfill)
+    XCTWaiter().wait(for: [expectation], timeout: 3)
+
+    // then
+    let window = UIApplication.shared.windows.first
+    let navigationController = window?.rootViewController as? UINavigationController
+    let rootViewController = navigationController?.viewControllers.first
+    XCTAssert(rootViewController is ProfileViewController)
   }
 
   func testLoginFailure_wrongUsername_presentAlertController() {
+    // given
+    _ = self.viewController.view // loadView
+    self.viewController.usernameField.text = "WRONG USERNAME"
+    self.viewController.passwordField.text = "campus123"
+
+    // when
+    self.viewController.login()
+
+    // wait
+    let expectation = XCTestExpectation()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: expectation.fulfill)
+    XCTWaiter().wait(for: [expectation], timeout: 3)
+
+    // then
+    let alertController = self.viewController.presentedViewController as? UIAlertController
+    XCTAssertNotNil(alertController)
+    XCTAssert(alertController?.message?.lowercased().contains("no such user") == true)
   }
 
   func testLoginFailure_wrongPassword_presentAlertController() {
+    // given
+    _ = self.viewController.view // loadView
+    self.viewController.usernameField.text = "fast"
+    self.viewController.passwordField.text = "WRONG PASSWORD"
+
+    // when
+    self.viewController.login()
+
+    // wait
+    let expectation = XCTestExpectation()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: expectation.fulfill)
+    XCTWaiter().wait(for: [expectation], timeout: 3)
+
+    // then
+    let alertController = self.viewController.presentedViewController as? UIAlertController
+    XCTAssertNotNil(alertController)
+    XCTAssert(alertController?.message?.lowercased().contains("wrong password") == true)
   }
 }
